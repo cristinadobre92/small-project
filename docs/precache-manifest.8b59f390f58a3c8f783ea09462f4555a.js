eeca1571a067fff9a357773a8ba230efd85d0dc5
Cristina Dobre <crisfdobre@gmail.com>
2020-06-12 12:09:18 +0200

    first commit

---
+++ docs/precache-manifest.8b59f390f58a3c8f783ea09462f4555a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a96b34902553a7ee1f08e7596052e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85b04fc9501151463020",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1b87c656c1fef47949e9",
+    "url": "/static/js/2.d68542a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d68542a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b04fc9501151463020",
+    "url": "/static/js/main.f0aea875.chunk.js"
+  },
+  {
+    "revision": "a81fee32fc1dd07bc9ee",
+    "url": "/static/js/runtime-main.5ca6114e.js"
+  }
+]);
